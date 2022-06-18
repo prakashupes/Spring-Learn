70cf51c6dd1c70ae9ed927e252a8b1578d339b09
prakashupes <85prakash2017@gmail.com>
2022-06-18 23:34:29 +0530

    Annotations @component

--- annonations/src/com/annonations/pracise/MyApp.java
+++ annonations/src/com/annonations/pracise/MyApp.java
@@ -6,7 +6,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach coachObj = context.getBean("myCoach",Coach.class);
+		Coach coachObj = context.getBean("tennisCoach",Coach.class);
 		System.out.println(coachObj.getDailyWorkout());
 		context.close();
 		
--- annonations/src/com/annonations/pracise/TennisCoach.java
+++ annonations/src/com/annonations/pracise/TennisCoach.java
@@ -2,9 +2,15 @@ package com.annonations.pracise;
 
 import org.springframework.stereotype.Component;
 
-@Component("myCoach") 
+//  @Component("myCoach")  
+
+//above is expicit bean id declaration
 //As previously we were creating the bean for that class and assigning IDs now myCoah use as bean id since 
 //spring automatically create a bean and assign the id as coach.
+
+//If we do not provide the bean id to component then it will automatic assign the bean ID as  tennisCoach
+
+@Component
 public class TennisCoach implements Coach{
 	public String getDailyWorkout() {
 		return "Tennis practise";
