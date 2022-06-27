69d6738e82cdb33263dfcdde035553ba3a7929fa
prakashupes <85prakash2017@gmail.com>
2022-06-27 11:34:33 +0530

    property file

--- JavaConfigsWithoutXML/src/com/javaconfig/practise/SportConfig.java
+++ JavaConfigsWithoutXML/src/com/javaconfig/practise/SportConfig.java
@@ -2,6 +2,7 @@ package com.javaconfig.practise;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /*
  * 1.This class will work as exactly same as XML file, We have configured and set @ComponentScan
@@ -10,8 +11,10 @@ import org.springframework.context.annotation.Configuration;
  * 2. We can also use auto wired and it will act like XML
  */
 
+
 @Configuration
 @ComponentScan("com.javaconfig.practise")  //If we will use this we no need to define bean
+@PropertySource("classpath:javaConfig.properties")
 public class SportConfig {
 
 }
--- JavaConfigsWithoutXML/src/com/javaconfig/practise/SwimCoach.java
+++ JavaConfigsWithoutXML/src/com/javaconfig/practise/SwimCoach.java
@@ -1,6 +1,7 @@
 package com.javaconfig.practise;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,11 @@ public class SwimCoach implements Coach{
 	@Autowired
 	private MyFortune myFortune;
 	
-	
+	@Value("Note: Values reading from property fle") //we can also hardcode the value
+	private String note;
+	@Value("${email}")
+	private String email;
+
 	@Override
 	public String Mytasks() {
 		
@@ -19,6 +24,10 @@ public class SwimCoach implements Coach{
 	public String getMyFrotune()
 	{
 		return myFortune.getFortune();
+	}
+	public String getEmail()
+	{
+		return this.note+" --->"+this.email;
 	}
 	
 
